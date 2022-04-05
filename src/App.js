fb4f1ba1668ce0092001d2845477c7a95a1cdf94
Rabi Shankar Prasad <rabiprasad607@gmail.com>
2022-04-05 20:09:18 +0530

    feat[navbar]:add navbar and its buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
